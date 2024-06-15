54a1b251ff4a524c95a5656b8952263a737a2c1b
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-06-16 00:19:44 +0100

    Adding WebSocketTestClient project

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using (ClientWebSocket webSocket = new ClientWebSocket())
+        {
+            Uri serverUri = new Uri("ws://localhost:5000/ws/");
+            await webSocket.ConnectAsync(serverUri, CancellationToken.None);
+            Console.WriteLine("Connected to WebSocket server");
+
+            await ReceiveMessages(webSocket);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+
+    private static async Task ReceiveMessages(ClientWebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4];
+        while (webSocket.State == WebSocketState.Open)
+        {
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
+                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                Console.WriteLine("WebSocket connection closed.");
+            }
+            else
+            {
+                var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                Console.WriteLine("Received: " + message);
+            }
+        }
+    }
+}
+
